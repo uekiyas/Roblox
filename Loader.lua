967155864a32b5ee581f5d464a5fd629cda97dcc
uekiyas <elechroma21@gmail.com>
2026-07-20 19:15:40 -0400

    update this later

---
+++ Loader.lua
@@ -0,0 +1,11 @@
+local ohmygodtimeandplace = ...
+
+local andyoudiditatmybirthdaydinner = {
+    ["DWP - Admin Panel"] = "a",
+    ["DWP - Item Giver"] = "a",
+    ["DWP - No Ability CD"] = "a"
+}
+
+if andyoudiditatmybirthdaydinner[ohmygodtimeandplace] then
+    loadstring(game:HttpGet(andyoudiditatmybirthdaydinner[ohmygodtimeandplace]))()
+end
