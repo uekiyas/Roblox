33b11de03e0c2f0d6d24a9c2719f2e3bf7775935
uekiyas <elechroma21@gmail.com>
2026-07-29 19:22:19 -0400

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,9 +1,7 @@
 local ohmygodtimeandplace = ...
 
 local andyoudiditatmybirthdaydinner = {
-    ["DWP - Admin Panel"] = "https://github.com/uekiyas/Roblox/raw/refs/heads/main/DandysWorldPlusAdminPanelRuns.lua",
-    ["DWP - No Ability CD"] = "https://github.com/uekiyas/Roblox/raw/refs/heads/main/DandysWorldPlusNoCoolDownAbilities.lua",
-    ["DWP - Admin Panel Auto Execute"] = "https://github.com/uekiyas/Roblox/raw/refs/heads/main/PrayPermitTheAutomaticExecutionOfTheMostEsteemedDandysWorldPlusAdministrativePanel.lua",
+    ["MMV"] = "https://github.com/uekiyas/Roblox/raw/refs/heads/main/MMV.lua",
 }
 
 if andyoudiditatmybirthdaydinner[ohmygodtimeandplace] then
